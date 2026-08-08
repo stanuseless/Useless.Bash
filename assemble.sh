560614b17d2cd94479910943fb6a847b3c1e63a2
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-08 15:51:40 +0300

    assemble

--- assemble.sh
+++ assemble.sh
@@ -1,8 +1,18 @@
 #!/usr/local/bin/bash
 
+if [[ $# -ne 1 ]]; then
+ echo 'Wrong arguments!' >&2; exit 1; fi
+
 REP_OWNER='stanuseless'
 REP_NAME='Useless.Bash'
 VERSION='0.4.1'
+
+BUILD_TYPE="$1"
+case "${BUILD_TYPE}" in
+ 'unstable') VERSION_NAME="${VERSION}-UNSTABLE";;
+ '') echo 'No build type!' >&2; exit 1;;
+ *) echo "Build type \"${BUILD_TYPE}\" is not supported!" >&2; exit 1;;
+esac
 
 if [[ -d 'build' ]]; then
  echo 'Build dir exists!' >&2; exit 1; fi
@@ -13,7 +23,7 @@ SUBJECT='build/yml/metadata.yml'
 echo "repository:
  owner: '${REP_OWNER}'
  name: '${REP_NAME}'
-version: '${VERSION}'" > "${SUBJECT}"
+version: '${VERSION_NAME}'" > "${SUBJECT}"
 
 if [[ ! -s 'LICENSE' ]]; then
  echo 'No license!' >&2; exit 1; fi
@@ -22,7 +32,7 @@ if [[ ! -s 'README.md' ]]; then
  echo 'No readme!' >&2; exit 1; fi
 
 mkdir -p 'build/zip'
-SUBJECT="build/zip/${REP_NAME}-${VERSION}.zip"
+SUBJECT="build/zip/${REP_NAME}-${VERSION_NAME}.zip"
 zip -Xqr9 "${SUBJECT}" 'src/main/bash' 'LICENSE' 'README.md'
 if [[ $? -ne 0 ]]; then
  echo 'Zip error!' >&2; exit 1; fi
